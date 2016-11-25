d3868f3a62ffdd9bec01b394a3d3037aa74394c4
Viacheslav Suchkov <viacheslavsuchkov@Viacheslavs-MacBook-Pro.local>
2016-11-25 17:27:34 +0200

    AF: Markup

--- frontend/components/Jumbotron/index.js
+++ frontend/components/Jumbotron/index.js
@@ -3,12 +3,9 @@ import React from 'react';
 class Jumbotron extends React.Component {
 	render() {
 		return (
-			<div class="jumbotron">
-				<div class="container">
-					<h1>Вас приветствует Афон</h1>
-					<p>Все гарячие мероприятия в городе. Изучите и расшарьте.</p>
-					<p><a class="btn btn-primary btn-lg" href="#" role="button">О нас</a></p>
-				</div>
+			<div class="jumbotron container AppHeader">
+				<p class="AppHeader-Greeting">Афон. Все гарячие мероприятия в городе {'<3'}</p>
+				{/*<p><a class="btn btn-primary btn-lg" href="#" role="button">О нас</a></p> */}
 			</div>
 		)
 	}
